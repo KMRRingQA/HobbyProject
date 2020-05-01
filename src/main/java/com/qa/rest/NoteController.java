f64027ee40692b87db0fffb26a132d66cf310d70
tvaidotas <tadas.vaidotas@gmail.com>
2020-05-01 11:51:27 +0100

    added an endpoint for getting all of the notes

--- src/main/java/com/qa/rest/NoteController.java
+++ src/main/java/com/qa/rest/NoteController.java
@@ -1,9 +1,27 @@
 package com.qa.rest;
 
+import com.qa.domain.Note;
+import com.qa.service.NoteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class NoteController {
+
+    private final NoteService service;
+
+    @Autowired
+    public NoteController(NoteService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/getAllNotes")
+    public List<Note> getAllNotes(){
+        return this.service.readNotes();
+    }
 
 
 
