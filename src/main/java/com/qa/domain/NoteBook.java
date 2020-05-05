d1de012269d334d02f176e758e8a46e2e8ab0750
tvaidotas <tadas.vaidotas@gmail.com>
2020-05-05 09:46:12 +0100

    added notebook domain added notebook repository

---
+++ src/main/java/com/qa/domain/NoteBook.java
@@ -0,0 +1,38 @@
+package com.qa.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class NoteBook {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    public NoteBook() {
+    }
+
+    public NoteBook(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- src/main/java/com/qa/repo/NoteBookRepository.java
+++ src/main/java/com/qa/repo/NoteBookRepository.java
@@ -1,9 +1,9 @@
 package com.qa.repo;
 
-import com.qa.domain.Note;
+import com.qa.domain.NoteBook;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface NotesRepository extends JpaRepository<Note, Long> {
+public interface NoteBookRepository extends JpaRepository<NoteBook, Long> {
 }
