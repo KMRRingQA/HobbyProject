3f10a2d87cb4d776faaf4d1b131c4ecec88642f5
tvaidotas <tadas.vaidotas@gmail.com>
2020-05-05 13:56:30 +0100

    reworked the NoteBookController

--- src/main/java/com/qa/rest/NoteBookController.java
+++ src/main/java/com/qa/rest/NoteBookController.java
@@ -2,9 +2,12 @@ package com.qa.rest;
 
 import com.qa.domain.Note;
 import com.qa.domain.NoteBook;
+import com.qa.dto.NoteBookDTO;
 import com.qa.service.NoteBookService;
 import com.qa.service.NoteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.websocket.server.PathParam;
@@ -21,33 +24,35 @@ public class NoteBookController {
     }
 
     @GetMapping("/getAllNoteBooks")
-    public List<NoteBook> getAllNotes(){
-        return this.service.readNoteBooks();
+    public ResponseEntity<List<NoteBookDTO>> getAllNotes(){
+        return new ResponseEntity<List<NoteBookDTO>>(this.service.readNoteBooks(), HttpStatus.ACCEPTED);
     }
 
     @PostMapping("/createNoteBook")
-    public NoteBook createNote(@RequestBody NoteBook note){
-        return this.service.createNoteBook(note);
+    public ResponseEntity<NoteBookDTO> createNote(@RequestBody NoteBook note){
+        return new ResponseEntity<NoteBookDTO>(this.service.createNoteBook(note), HttpStatus.CREATED);
     }
 
     @DeleteMapping("/deleteNoteBook/{id}")
-    public boolean deleteNote(@PathVariable Long id){
-        return this.service.deleteNoteBook(id);
+    public ResponseEntity<?> deleteNote(@PathVariable Long id){
+        return this.service.deleteNoteBook(id)
+            ? ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build()
+            : ResponseEntity.noContent().build();
     }
 
     @GetMapping("/getNoteBookById/{id}")
-    public NoteBook getNoteById(@PathVariable Long id){
-        return this.service.findNoteBookById(id);
+    public ResponseEntity<NoteBookDTO> getNoteById(@PathVariable Long id){
+        return ResponseEntity.ok(this.service.findNoteBookById(id));
     }
 
     @PutMapping("/updateNoteBook/{id}")
-    public NoteBook updateNote(@PathVariable Long id, @RequestBody NoteBook note){
-        return this.service.updateNoteBook(id, note);
+    public ResponseEntity<NoteBookDTO> updateNote(@PathVariable Long id, @RequestBody NoteBook note){
+        return ResponseEntity.ok(this.service.updateNoteBook(id, note));
     }
 
     @PutMapping("/updateNoteBook2")
-    public NoteBook updateNote2(@PathParam("id") Long id, @RequestBody NoteBook note){
-        return this.service.updateNoteBook(id, note);
+    public ResponseEntity<NoteBookDTO> updateNote2(@PathParam("id") Long id, @RequestBody NoteBook note){
+        return ResponseEntity.ok(this.service.updateNoteBook(id, note));
     }
 
 }
