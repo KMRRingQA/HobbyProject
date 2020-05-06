39d195799d6f990ca0ea531759c788acc6ddd90b
tvaidotas <tadas.vaidotas@gmail.com>
2020-05-06 17:19:09 +0100

    added the get all notes test

--- src/test/java/com/qa/rest/NotesControllerIntegrationTest.java
+++ src/test/java/com/qa/rest/NotesControllerIntegrationTest.java
@@ -5,13 +5,24 @@ import com.qa.domain.Note;
 import com.qa.dto.NoteDTO;
 import com.qa.repo.NotesRepository;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.request;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -48,6 +59,21 @@ public class NotesControllerIntegrationTest {
         this.testNoteWithID = this.repository.save(testNote);
         this.id = testNoteWithID.getId();
         this.noteDTO = this.mapToDTO(testNoteWithID);
+    }
+
+    @Test
+    public void getAllNotesTest() throws Exception {
+        List<NoteDTO> noteDTOList = new ArrayList<>();
+        noteDTOList.add(noteDTO);
+        String content = this.mock.perform(
+            request(HttpMethod.GET, "/getAllNotes")
+                .accept(MediaType.APPLICATION_JSON)
+        )
+            .andExpect(status().isOk())
+            .andReturn()
+            .getResponse()
+            .getContentAsString();
+        assertEquals(content, this.objectMapper.writeValueAsString(noteDTOList));
     }
 
 }
