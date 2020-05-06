ddf2214e406d166029b5947261a1cfb25b374171
tvaidotas <tadas.vaidotas@gmail.com>
2020-05-06 13:25:19 +0100

    added NoteServiceUnitTest class

---
+++ src/test/java/com/qa/service/NoteServiceUnitTest.java
@@ -0,0 +1,12 @@
+package com.qa.service;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class NoteServiceUnitTest {
+
+
+
+
+}
