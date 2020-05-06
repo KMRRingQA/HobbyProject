7e76f6936c2a2d6d4476a0da5ed60219a09ea3b3
tvaidotas <tadas.vaidotas@gmail.com>
2020-05-06 17:02:12 +0100

    added the before method and initialised the fields

--- src/test/java/com/qa/rest/NotesControllerIntegrationTest.java
+++ src/test/java/com/qa/rest/NotesControllerIntegrationTest.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.qa.domain.Note;
 import com.qa.dto.NoteDTO;
 import com.qa.repo.NotesRepository;
+import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -40,6 +41,13 @@ public class NotesControllerIntegrationTest {
         return this.mapper.map(note, NoteDTO.class);
     }
 
-
+    @Before
+    public void setUp(){
+        this.repository.deleteAll();
+        this.testNote = new Note("test note", "test description");
+        this.testNoteWithID = this.repository.save(testNote);
+        this.id = testNoteWithID.getId();
+        this.noteDTO = this.mapToDTO(testNoteWithID);
+    }
 
 }
