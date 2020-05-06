091ea495b272d525d87fe5d4fbb9f0e263999b77
tvaidotas <tadas.vaidotas@gmail.com>
2020-05-06 16:17:23 +0100

    added the NotesControllerUnitTest class

---
+++ src/test/java/com/qa/rest/NotesControllerUnitTest.java
@@ -0,0 +1,12 @@
+package com.qa.rest;
+
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class NotesControllerUnitTest {
+
+
+
+
+}
