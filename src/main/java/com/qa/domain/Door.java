a7096f756bcecfaba06ea20130306f02be663116
Korbinian <kmr.ring@outlook.com>
2020-05-18 10:47:38 +0100

    added 4 parameters to door

--- src/main/java/com/qa/domain/Door.java
+++ src/main/java/com/qa/domain/Door.java
@@ -1,6 +1,7 @@
 package com.qa.domain;
 
 import javax.persistence.*;
+import java.math.BigDecimal;
 import java.util.Objects;
 
 @Entity
@@ -14,16 +15,56 @@ public class Door {
     private String title;
     private String description;
 
+    private String value1;
+    private String value2;
+    private String value3;
+    private BigDecimal cost;
+
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
-//  @JoinColumn(name = "note_book_id")
     private Manufacturer manufacturer;
 
     public Door() {
     }
 
-    public Door(String title, String description) {
+    public String getValue1() {
+        return value1;
+    }
+
+    public void setValue1(String value1) {
+        this.value1 = value1;
+    }
+
+    public String getValue2() {
+        return value2;
+    }
+
+    public void setValue2(String value2) {
+        this.value2 = value2;
+    }
+
+    public String getValue3() {
+        return value3;
+    }
+
+    public void setValue3(String value3) {
+        this.value3 = value3;
+    }
+
+    public BigDecimal getCost() {
+        return cost;
+    }
+
+    public void setCost(BigDecimal cost) {
+        this.cost = cost;
+    }
+
+    public Door(String title, String description, String value1, String value2, String value3, BigDecimal cost) {
         this.title = title;
         this.description = description;
+        this.value1 = value1;
+        this.value2 = value2;
+        this.value3 = value3;
+        this.cost = cost;
     }
 
     public Long getId() {
@@ -59,35 +100,22 @@ public class Door {
     }
 
     @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Door other = (Door) obj;
-        if (description == null) {
-            if (other.description != null)
-                return false;
-        } else if (!description.equals(other.description))
-            return false;
-        if (id == null) {
-            if (other.id != null)
-                return false;
-        } else if (!id.equals(other.id))
-            return false;
-        if (title == null) {
-            if (other.title != null)
-                return false;
-        } else if (!title.equals(other.title))
-            return false;
-        return true;
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Door door = (Door) o;
+        return Objects.equals(id, door.id) &&
+                Objects.equals(title, door.title) &&
+                Objects.equals(description, door.description) &&
+                Objects.equals(value1, door.value1) &&
+                Objects.equals(value2, door.value2) &&
+                Objects.equals(value3, door.value3) &&
+                Objects.equals(cost, door.cost) &&
+                Objects.equals(manufacturer, door.manufacturer);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, title, description);
+        return Objects.hash(id, title, description, value1, value2, value3, cost, manufacturer);
     }
-
 }
