10aa4352d8b2b0fb95bc84fe1283bc65113be584
Korbinian <kmr.ring@outlook.com>
2020-05-19 18:59:43 +0100

    testing with selenium

--- src/test/java/com/qa/selenium/AppTest.java
+++ src/test/java/com/qa/selenium/AppTest.java
@@ -1,5 +1,7 @@
 package com.qa.selenium;
-
+import com.qa.App;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.relevantcodes.extentreports.ExtentReports;
 import com.relevantcodes.extentreports.ExtentTest;
 import com.relevantcodes.extentreports.LogStatus;
@@ -28,7 +30,7 @@ public class AppTest
     ExtentTest test;
 
     @BeforeTest
-    public void startReport(){
+    public void startReport() throws InterruptedException {
         report = new ExtentReports(
                 System.getProperty("user.dir") + "/test-output/Report.html",
                 true
@@ -37,6 +39,8 @@ public class AppTest
                 .addSystemInfo("Host Name", "QA")
                 .addSystemInfo("Tester", "Tadas");
         report.loadConfig(new File(System.getProperty("user.dir") + "\\extent-report.xml"));
+        SpringApplication.run(App.class);
+        sleep(10000);
     }
 
     @BeforeMethod
@@ -49,92 +53,16 @@ public class AppTest
         test = report.startTest("buying cheapest first class UA flight to paris");
         driver.manage().window().maximize();
         test.log(LogStatus.INFO, "Started chrome browser and made it fullscreen");
-        driver.get("http://www.newtours.demoaut.com/");
+        driver.get("http://localhost:8181/");
+        sleep(5000);
         test.log(LogStatus.INFO, "Started chrome browser and made it fullscreen");
-        WebElement registerButton = (new WebDriverWait(driver, 15)).until(ExpectedConditions.presenceOfElementLocated(By.xpath("/html/body/div/table/tbody/tr/td[2]/table/tbody/tr[2]/td/table/tbody/tr/td[2]/a")));
-        registerButton.click();
+        WebElement browse = (new WebDriverWait(driver, 1000)).until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#menu > ul > li:nth-child(2) > a")));
+        browse.click();
 
-        WebElement userName = (new WebDriverWait(driver, 15)).until(ExpectedConditions.presenceOfElementLocated(By.id("email")));
-        userName.sendKeys("CirbysTestAccount" + (int) (Math.random() * 10000 + 10000));
-        WebElement password = (new WebDriverWait(driver, 15)).until(ExpectedConditions.presenceOfElementLocated(By.name("password")));
-        password.sendKeys("asdfasdf");
-        WebElement password2 = (new WebDriverWait(driver, 15)).until(ExpectedConditions.presenceOfElementLocated(By.name("confirmPassword")));
-        password2.sendKeys("asdfasdf");
-        WebElement registerButton2 = (new WebDriverWait(driver, 15)).until(ExpectedConditions.presenceOfElementLocated(By.name("register")));
-        registerButton2.click();
-        test.log(LogStatus.INFO, "registered account");
+        WebElement search = (new WebDriverWait(driver, 1000)).until(ExpectedConditions.presenceOfElementLocated(By.id("search")));
 
-        sleep(500);
 
-        WebElement flightsButton = (new WebDriverWait(driver, 15)).until(ExpectedConditions.presenceOfElementLocated(By.xpath("/html/body/div/table/tbody/tr/td[1]/table/tbody/tr/td/table/tbody/tr/td/table/tbody/tr[2]/td[2]/a")));
-        flightsButton.click();
-
-        WebElement oneWayButton = (new WebDriverWait(driver, 15)).until(ExpectedConditions.presenceOfElementLocated(By.xpath("/html/body/div/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[2]/td[2]/b/font/input[2]")));
-        oneWayButton.click();
-
-        WebElement passengerSelect = (new WebDriverWait(driver, 10)).until(ExpectedConditions.presenceOfElementLocated(By.name("passCount")));
-        Select passengerSelector = new Select(driver.findElement(By.name("passCount")));
-        passengerSelector.selectByValue(String.valueOf(2));
-
-        WebElement departureSelect = (new WebDriverWait(driver, 10)).until(ExpectedConditions.presenceOfElementLocated(By.name("fromPort")));
-        Select departureSelector = new Select(driver.findElement(By.name("fromPort")));
-        departureSelector.selectByValue("London");
-
-        WebElement arrivalSelect = (new WebDriverWait(driver, 10)).until(ExpectedConditions.presenceOfElementLocated(By.name("toPort")));
-        Select arrivalSelector = new Select(driver.findElement(By.name("toPort")));
-        arrivalSelector.selectByValue("Paris");
-
-        WebElement firstClassButton = (new WebDriverWait(driver, 15)).until(ExpectedConditions.presenceOfElementLocated(By.xpath("/html/body/div/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[9]/td[2]/font/font/input[2]")));
-        firstClassButton.click();
-
-        WebElement airLineSelect = (new WebDriverWait(driver, 10)).until(ExpectedConditions.presenceOfElementLocated(By.name("airline")));
-        Select airLineSelector = new Select(driver.findElement(By.name("airline")));
-        airLineSelector.selectByVisibleText("Unified Airlines");
-
-        test.log(LogStatus.INFO, "selected flight details");
-
-        sleep(500);
-
-        WebElement continueButton = (new WebDriverWait(driver, 15)).until(ExpectedConditions.presenceOfElementLocated(By.name("findFlights")));
-        continueButton.click();
-
-        WebElement continueButton2 = (new WebDriverWait(driver, 15)).until(ExpectedConditions.presenceOfElementLocated(By.name("reserveFlights")));
-        continueButton2.click();
-
-        test.log(LogStatus.INFO, "continued to checkout");
-
-        sleep(500);
-
-        WebElement firstName1 = (new WebDriverWait(driver, 15)).until(ExpectedConditions.presenceOfElementLocated(By.name("passFirst0")));
-        firstName1.sendKeys("asdf");
-
-        WebElement lastName1 = (new WebDriverWait(driver, 15)).until(ExpectedConditions.presenceOfElementLocated(By.name("passLast0")));
-        lastName1.sendKeys("asdf");
-
-        WebElement firstName2 = (new WebDriverWait(driver, 15)).until(ExpectedConditions.presenceOfElementLocated(By.name("passFirst1")));
-        firstName2.sendKeys("asdf");
-
-        WebElement lastName2 = (new WebDriverWait(driver, 15)).until(ExpectedConditions.presenceOfElementLocated(By.name("passLast1")));
-        lastName2.sendKeys("asdf");
-
-        WebElement numberInput = (new WebDriverWait(driver, 15)).until(ExpectedConditions.presenceOfElementLocated(By.name("creditnumber")));
-        numberInput.sendKeys("1235324234");
-
-        WebElement purchaseButton = (new WebDriverWait(driver, 15)).until(ExpectedConditions.presenceOfElementLocated(By.name("buyFlights")));
-        purchaseButton.click();
-
-        test.log(LogStatus.INFO, "entered passenger details");
-
-        sleep(500);
-
-        WebElement success = (new WebDriverWait(driver, 15)).until(ExpectedConditions.presenceOfElementLocated(By.xpath("/html/body/div/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr[1]/td[2]/table/tbody/tr[3]/td/p/font/b/font[2]")));
-
-        File bookedOrder = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-        FileUtils.copyFile(bookedOrder, new File(System.getProperty("user.dir") + "/test-output/bookedOrder.jpg"));
-        test.log(LogStatus.INFO, "took screenshot of booked order");
-        test.log(LogStatus.PASS, "booked order successful", "<img src=bookedOrder.jpg>");
-
-        assertEquals(success.getText(),"Your itinerary has been booked!");
+        assertEquals(search.getText(),"SEARCH");
 
     }
 
